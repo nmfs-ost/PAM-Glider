265c9edb01d22068281790ac6238cadc07764db5
Kourtney Burger <kourtney.burger@noaa.gov>
2025-05-08 11:23:16 -0700

    Added data pre-processing methods

---
+++ content/data_processing/_methods/wispr-decompress.R
@@ -0,0 +1,12 @@
+#' ---
+#' Step 2. Decompress Data and Convert to Usable Audio Files
+#' 
+#' Conversion is done using the **Agate WISPR workflow** (`workflow_convertWISPR.m`) to convert `.dat` files to `.flac`.
+#' The following glider-specific configuration has been prepared:
+#' 
+#' - **Deployment ID**: {{deployment_id}}
+#' - **Glider Serial Number**: {{glider_id}}
+#' - **Input Directory**: {{input_dir}}
+#' - **Output Directory**: {{output_dir}}
+#' 
+#' ---
